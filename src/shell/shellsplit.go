62ba240b27f62efa39d077a4bd6dd366469af340
Makoto Onuki <omakoto@google.com>
2018-05-05 11:21:17 -0700

    Improving shellsplit

--- src/shell/shellsplit.go
+++ src/shell/shellsplit.go
@@ -7,127 +7,141 @@ import (
 // Shell tokenizer.
 // TODO Handle operators such as |.
 
-type splitter struct {
-	text []rune
+type Token struct {
+	Word  string
+	Index int
+}
 
+type splitter struct {
 	// Input.
+	text []rune
 	next int
 
+	// State
 	wasSpecial bool
+	start      int
+	buffer     bytes.Buffer
+	hasRunes   bool
 
-	// Output.
-	buffer   bytes.Buffer
-	hasRunes bool
-
-	result []string
+	// Result
+	resultTokens []Token
 }
 
 func newSplitter(text string) splitter {
-	return splitter{[]rune(text), 0, false, bytes.Buffer{}, false, make([]string, 0, 16)}
-}
-
-func (s *splitter) peek() (rune, bool) {
-	if s.next < len(s.text) {
-		return s.text[s.next], true
+	return splitter{
+		text:         []rune(text),
+		buffer:       bytes.Buffer{},
+		start:        -1,
+		resultTokens: make([]Token, 0),
 	}
-	return '\x00', false
 }
 
-func (s *splitter) read() (rune, bool) {
-	r, ok := s.peek()
+func (s *splitter) peek() (rune, bool, int) {
+	if s.next < len(s.text) {
+		return s.text[s.next], true, s.next
+	}
+	return '\x00', false, -1
+}
+
+func (s *splitter) read() (rune, bool, int) {
+	r, ok, index := s.peek()
 
 	if ok {
 		s.next++
 	}
-	return r, ok
+	return r, ok, index
 }
 
-func (s *splitter) pushRuneNoSpecial(r rune) {
+func (s *splitter) pushRuneNoSpecial(r rune, index int) {
 	s.buffer.WriteRune(r)
 	s.hasRunes = true
+	if s.start < 0 {
+		s.start = index
+	}
 }
 
-func (s *splitter) pushRune(r rune) {
-	s.pushRuneNoSpecial(r)
+func (s *splitter) pushRune(r rune, index int) {
+	s.pushRuneNoSpecial(r, index)
 	s.wasSpecial = isSpecialChar(r)
 }
 
-func (s *splitter) pushWord() {
+func (s *splitter) onWordBoundary() {
 	if s.hasRunes {
+		s.resultTokens = append(s.resultTokens, Token{s.buffer.String(), s.start})
 		s.hasRunes = false
-		s.result = append(s.result, s.buffer.String())
-		s.buffer = bytes.Buffer{}
+		s.start = -1
 		s.wasSpecial = false
+		s.buffer = bytes.Buffer{}
 	}
 }
 
 func (s *splitter) eatSingleQuote() {
 	for {
-		r, ok := s.read()
+		r, ok, index := s.read()
 		if !ok {
 			return
 		}
 		if r == '\'' {
-			s.pushRune(r)
+			s.pushRune(r, index)
 			return
 		}
-		s.pushRune(r)
+		s.pushRune(r, index)
 	}
 }
 
 func (s *splitter) eatDoubleQuote(end rune) {
 	for {
-		r, ok := s.read()
+		r, ok, index := s.read()
 		if !ok {
 			return
 		}
 		if r == end {
-			s.pushRune(r)
+			s.pushRune(r, index)
 			return
 		}
-		if s.maybeEatDollar(r) {
+		if s.maybeEatDollar(r, index) {
 			continue
 		}
 		if r == '\\' {
-			s.pushRuneNoSpecial(r)
-			r, ok = s.read()
+			s.pushRuneNoSpecial(r, index)
+			r, ok, _ = s.read()
 			if !ok {
 				return
 			}
 		}
-		s.pushRune(r)
+		s.pushRune(r, index)
 	}
 }
 
-func (s *splitter) maybeEatDollar(r rune) bool {
+func (s *splitter) maybeEatDollar(r rune, index int) bool {
 	if r == '$' {
-		s.pushRune(r)
+		s.pushRune(r, index)
 
-		next, ok := s.peek()
+		next, ok, index := s.peek()
 		if !ok || isWhitespace(next) {
 			return true
 		}
 		if next == '(' {
 			s.read()
-			s.pushRuneNoSpecial(next)
+			s.pushRuneNoSpecial(next, index)
 			s.tokenize(')')
 			return true
 		}
 		if next == '{' {
 			s.read()
-			s.pushRuneNoSpecial(next)
+			s.pushRuneNoSpecial(next, index)
 			s.tokenize('}')
 			return true
 		}
 		if next == '\'' {
 			s.read()
-			s.pushRuneNoSpecial(next)
+			s.pushRuneNoSpecial(next, index)
 			s.eatSingleQuote()
 			return true
 		}
 		if next == '"' {
 			s.read()
-			s.pushRuneNoSpecial(next)
+			s.pushRuneNoSpecial(next, index)
 			s.eatDoubleQuote('"')
 			return true
 		}
@@ -162,72 +176,84 @@ func isCommandSeparatorChar(r rune) bool {
 
 func (s *splitter) tokenize(end int) {
 	for {
-		r, ok := s.read()
+		r, ok, index := s.read()
 		if !ok {
 			break
 		}
 		if end >= 0 && end == int(r) {
-			s.pushRuneNoSpecial(r)
+			s.pushRuneNoSpecial(r, index)
 			break
 		}
 		if end < 0 && isWhitespace(r) {
-			s.pushWord()
+			s.onWordBoundary()
 			continue
 		}
 		if s.wasSpecial != isSpecialChar(r) {
-			s.pushWord()
+			s.onWordBoundary()
 		}
 		if r == '\\' {
-			s.pushRune(r)
-			r, ok = s.read()
+			s.pushRune(r, index)
+			r, ok, index = s.read()
 			if !ok {
 				break
 			}
-			s.pushRune(r)
+			s.pushRune(r, index)
 			continue
 		}
 		if r == '\'' {
-			s.pushRune(r)
+			s.pushRune(r, index)
 			s.eatSingleQuote()
 			continue
 		}
 		if r == '"' {
-			s.pushRune(r)
+			s.pushRune(r, index)
 			s.eatDoubleQuote('"')
 			continue
 		}
 		if r == '`' {
-			s.pushRune(r)
+			s.pushRune(r, index)
 			s.eatDoubleQuote('`')
 			continue
 		}
-		if s.maybeEatDollar(r) {
+		if s.maybeEatDollar(r, index) {
 			continue
 		}
+		// If # follows a whitespace,the rest is a comment.
 		if !s.hasRunes && r == '#' {
 			for {
-				s.pushRuneNoSpecial(r)
-				r, ok = s.read()
+				s.pushRuneNoSpecial(r, index)
+				r, ok, index = s.read()
 				if !ok {
-					s.pushWord()
+					s.onWordBoundary()
 					return
 				}
 			}
 		}
-		s.pushRune(r)
+		s.pushRune(r, index)
 	}
 	if end < 0 {
-		s.pushWord()
+		s.onWordBoundary()
 	}
 }
 
 // Split splits a whole command line into tokens.
 // Example: "cat fi\ le.txt|grep -V ^# >'out$$.txt' # Find non-comment lines."
-// -> output: cat, fi\ le.txt, |, grep, -V, ^#, >, out.txt ,# Find non-comment lines.
+// -> output: cat, fi\ le.txt, |, grep, -V, ^#, >, 'out$$.txt', # Find non-comment lines.
 func Split(text string) []string {
 	s := newSplitter(text)
 	s.tokenize(-1)
-	return s.result
+
+	ret := make([]string, 0, len(s.resultTokens))
+	for _, s := range s.resultTokens {
+		ret = append(ret, s.Word)
+	}
+	return ret
+}
+
+func SplitToTokens(text string) []Token {
+	s := newSplitter(text)
+	s.tokenize(-1)
+	return s.resultTokens
 }
 
 func IsCommandSeparator(text string) bool {
--- src/shell/shellsplit_test.go
+++ src/shell/shellsplit_test.go
@@ -39,3 +39,56 @@ func TestShellSplit(t *testing.T) {
 		assert.Equal(t, v.expected, actual, "Source="+v.source)
 	}
 }
+
+func ai(vals ...interface{}) []Token {
+	ret := make([]Token, 0)
+
+	start := 0
+
+	for _, v := range vals {
+		switch v.(type) {
+		case int:
+			start += v.(int)
+		case string:
+			s := v.(string)
+			ret = append(ret, Token{s, start})
+			start += len(s)
+		}
+	}
+	return ret
+}
+
+func TestShellSplitWithIndexes(t *testing.T) {
+	inputs := []struct {
+		source   string
+		expected []Token
+	}{
+		{"", ai()},
+		{"a", ai("a")},
+		{"aaa", ai("aaa")},
+		{"  aaa", ai(2, "aaa")},
+		{"  a", ai(2, "a")},
+		{"aaa b  ccc", ai("aaa", 1, "b", 2, "ccc")},
+		{"aaa 'b  b'  ccc", ai("aaa", 1, "'b  b'", 2, "ccc")},
+		{`aaa 'b  b'\''  d'  ccc`, ai("aaa", 1, `'b  b'\''  d'`, 2, "ccc")},
+		{"`ab\"`  ccc", ai("`ab\"`", 2, "ccc")},
+		{`a\ \'\ \"`, ai(`a\ \'\ \"`)},
+		{`$HOME/abc`, ai(`$HOME/abc`)},
+		{`${HOME}/abc`, ai(`${HOME}/abc`)},
+		{`  $(cat  ok  "$(next   "de  f")")/abc  xyz`, ai(2, `$(cat  ok  "$(next   "de  f")")/abc`, 2, "xyz")},
+		{`$ \`, ai(`$`, 1, `\`)},
+		{`$`, ai(`$`)},
+		{`$'xyz' abc`, ai(`$'xyz'`, 1, `abc`)},
+		{`$"xyz" abc`, ai(`$"xyz"`, 1, `abc`)},
+		{`"\`, ai(`"\`)},
+		{`'a x ;'     b`, ai(`'a x ;'`, 5, `b`)},
+		{`cat|&grep>&ab#def  # commenct;abc`, ai(`cat`, `|&`, `grep`, `>&`, `ab#def`, 2, `# commenct;abc`)},
+		{`echo $'a\xffb' # broken utf8`, ai(`echo`, 1, `$'a\xffb'`, 1, `# broken utf8`)},
+		{`cat fi\ le.txt|grep -V ^# >'out$$.txt' # Find non-comment lines.`,
+			ai(`cat`, 1, `fi\ le.txt`, `|`, `grep`, 1, `-V`, 1, `^#`, 1, `>`, `'out$$.txt'`, 1, `# Find non-comment lines.`)},
+	}
+	for _, v := range inputs {
+		actual := SplitToTokens(v.source)
+		assert.Equal(t, v.expected, actual, "Source="+v.source)
+	}
+}
